d9a6d9073f2ede1d8a746ad7fff99697c92a4fa8
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-06-02 23:42:37 -0400

    Write basic args parser using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use {
+	clap::{Parser, Subcommand}
+};
+
+#[derive(Subcommand)]
+enum Action {
+	Gen {
+		#[clap(long("charset"))]
+		character_set: Option<String>,
+
+		#[clap(long("no-caps"))]
+		no_capitals: bool,
+
+		#[clap(long("no-nums"))]
+		no_numbers: bool,
+
+		#[clap(long("no-symbols"))]
+		no_symbols: bool,
+	}
+}
+
+#[derive(Parser)]
+struct Args {
+	#[clap(subcommand)]
+	action: Action
+}
+
 fn main() {
-  println!("Hello, world!");
+	let args = Args::parse();
 }
